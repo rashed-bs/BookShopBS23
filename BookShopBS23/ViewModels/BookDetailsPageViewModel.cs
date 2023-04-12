14e473f083bc326e6b0c0b16382b215628131646
rasheduzzaman <mdrasheduzzamanakash@gmail.com>
2023-04-13 00:33:55 +0600

    Adds the Model, View Model Implementations.

---
+++ BookShopBS23/ViewModels/BookDetailsPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BookShopBS23.Models;
+
+namespace BookShopBS23.ViewModels
+{
+    public class BookDetailsPageViewModel
+    {
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+
+    }
+}
